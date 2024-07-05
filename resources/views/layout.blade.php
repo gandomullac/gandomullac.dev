ff2807276842a2d7b9737c4d6539e337252fd838
Claudio Gandini <claudio.gandini@tt-services.it>
2024-07-05 17:37:03 +0200

    Aggiunta meta tag

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,7 +6,7 @@
 
     <meta charset="UTF-8">
     <meta name="language" content="en">
-    <meta name="description" content="">
+    <meta name="description" content="gandomullac's portfolio">
     <meta name="keywords" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="content-type" content="text/html; charset=utf-8">
