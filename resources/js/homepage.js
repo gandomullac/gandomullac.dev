210555626bb7f5fa7c55f623a7411f81d1f5ae9c
Claudio Gandini <claudio.gandini@tt-services.it>
2024-07-08 17:38:45 +0200

    Aggiunta "Cløëd"

--- resources/js/homepage.js
+++ resources/js/homepage.js
@@ -14,6 +14,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 "Claude",
                 "Clod",
                 "Clœd",
+                "Cløëd",
                 "Clöd",
                 "Cloddo",
                 "Cloddoman",
