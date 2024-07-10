0a99d5a013df7184f849c8fa7facb99377a4f0f2
Claudio Gandini <claudio.gandini@tt-services.it>
2024-07-10 17:14:19 +0200

    Aggiornamento minore pagina about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -15,19 +15,20 @@
                 <article class="post__content">
 
                     <p>My name is Claudio and I'm a techy based in Ferrara</p>
-                    <p>I'm currently a consultant in a system engineer company, however I enjoy most working on developing
+                    <p>I'm currently a full-time consultant in a system engineer company, however I enjoy most working on
+                        developing
                         web apps for clients using my favorite stacks.</p>
 
                     <h2 id="interests">Interests<a class="anchor" href="#interests">#</a></h2>
                     <ul>
-                        <li>Web Development: I strive to code in a future-proof way, iteration after iteration.</li>
-                        <li>DevOps: I like to manage the whole stack, from that one missing semicolon to the CI/CD during
-                            deployment</li>
-                        <li>TDD: because it just makes so much sense.</li>
+                        <li>Web Development: I strive to write future-proof code.</li>
+                        <li>DevOps: I'm used to manage the whole stack, from that one missing semicolon to the CI/CD during
+                            deployment.</li>
+                        <li>TDD: because peace of mind is underestimated, and so are unit tests.</li>
                         <li>Obsidian.md: it is pretty awesome.</li>
                         <li>Cybersecurity: I played the part of the expert in a couple of courses I teached in.</li>
                         <li>IoT: both domestic and enterprise (I mostly like Philips Hue, they are so very nice).</li>
-                        <li>Gym, beach tennis, music and everything tech.</li>
+                        <li>Gym, beach tennis, cycling, music and everything tech.</li>
                     </ul>
 
                     @isset($curricula)
