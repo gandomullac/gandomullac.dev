0c36cbea433d73206fb612ea9712a01882ae1fd0
gandomullac <gandomullac@gmail.com>
2024-12-11 09:38:28 +0100

    Patch per hasValidSignature()

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Request;
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
@@ -11,7 +13,30 @@ class AppServiceProvider extends ServiceProvider
      * Register any application services.
      */
     public function register(): void
-    {    
+    {
+        UrlGenerator::macro('alternateHasCorrectSignature', function (Request $request, $absolute = true, array $ignoreQuery = []) {
+            $ignoreQuery[] = 'signature';
+
+            // ensure the base path is applied to absolute url
+            $absoluteUrl = url($request->path()); // forceRootUrl and forceScheme will apply
+            $url = $absolute ? $absoluteUrl : '/' . $request->path();
+
+            $queryString = collect(explode('&', (string) $request->server->get('QUERY_STRING')))
+                ->reject(fn($parameter) => in_array(Str::before($parameter, '='), $ignoreQuery))
+                ->join('&');
+            $original = rtrim($url . '?' . $queryString, '?');
+            $signature = hash_hmac('sha256', $original, call_user_func($this->keyResolver));
+            return hash_equals($signature, (string) $request->query('signature', ''));
+        });
+
+        UrlGenerator::macro('alternateHasValidSignature', function (Request $request, $absolute = true, array $ignoreQuery = []) {
+            return URL::alternateHasCorrectSignature($request, $absolute, $ignoreQuery)
+                && URL::signatureHasNotExpired($request);
+        });
+
+        Request::macro('hasValidSignature', function ($absolute = true, array $ignoreQuery = []) {
+            return URL::alternateHasValidSignature($this, $absolute, $ignoreQuery);
+        });
     }
 
     /**
