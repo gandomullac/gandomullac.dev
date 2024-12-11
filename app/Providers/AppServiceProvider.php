5c12b0d9ab24deaa30a0f6588c22b6740c7d03d6
gandomullac <gandomullac@gmail.com>
2024-12-11 10:02:47 +0100

    Tentativo content security policy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,11 @@
 
 namespace App\Providers;
 
+use Filament\Support\Facades\FilamentView;
 use Illuminate\Http\Request;
 use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\HtmlString;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
 
@@ -15,29 +17,29 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        UrlGenerator::macro('alternateHasCorrectSignature', function (Request $request, $absolute = true, array $ignoreQuery = []) {
-            $ignoreQuery[] = 'signature';
+        // UrlGenerator::macro('alternateHasCorrectSignature', function (Request $request, $absolute = true, array $ignoreQuery = []) {
+        //     $ignoreQuery[] = 'signature';
 
-            // ensure the base path is applied to absolute url
-            $absoluteUrl = url($request->path()); // forceRootUrl and forceScheme will apply
-            $url = $absolute ? $absoluteUrl : '/' . $request->path();
+        //     // ensure the base path is applied to absolute url
+        //     $absoluteUrl = url($request->path()); // forceRootUrl and forceScheme will apply
+        //     $url = $absolute ? $absoluteUrl : '/' . $request->path();
 
-            $queryString = collect(explode('&', (string) $request->server->get('QUERY_STRING')))
-                ->reject(fn($parameter) => in_array(Str::before($parameter, '='), $ignoreQuery))
-                ->join('&');
-            $original = rtrim($url . '?' . $queryString, '?');
-            $signature = hash_hmac('sha256', $original, call_user_func($this->keyResolver));
-            return hash_equals($signature, (string) $request->query('signature', ''));
-        });
+        //     $queryString = collect(explode('&', (string) $request->server->get('QUERY_STRING')))
+        //         ->reject(fn($parameter) => in_array(Str::before($parameter, '='), $ignoreQuery))
+        //         ->join('&');
+        //     $original = rtrim($url . '?' . $queryString, '?');
+        //     $signature = hash_hmac('sha256', $original, call_user_func($this->keyResolver));
+        //     return hash_equals($signature, (string) $request->query('signature', ''));
+        // });
 
-        UrlGenerator::macro('alternateHasValidSignature', function (Request $request, $absolute = true, array $ignoreQuery = []) {
-            return URL::alternateHasCorrectSignature($request, $absolute, $ignoreQuery)
-                && URL::signatureHasNotExpired($request);
-        });
+        // UrlGenerator::macro('alternateHasValidSignature', function (Request $request, $absolute = true, array $ignoreQuery = []) {
+        //     return URL::alternateHasCorrectSignature($request, $absolute, $ignoreQuery)
+        //         && URL::signatureHasNotExpired($request);
+        // });
 
-        Request::macro('hasValidSignature', function ($absolute = true, array $ignoreQuery = []) {
-            return URL::alternateHasValidSignature($this, $absolute, $ignoreQuery);
-        });
+        // Request::macro('hasValidSignature', function ($absolute = true, array $ignoreQuery = []) {
+        //     return URL::alternateHasValidSignature($this, $absolute, $ignoreQuery);
+        // });
     }
 
     /**
@@ -45,6 +47,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+
+        FilamentView::registerRenderHook(
+            'panels::head.start',
+            fn (): string => '<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">',
+        );
+
         URL::forceScheme('https');
     }
 }
