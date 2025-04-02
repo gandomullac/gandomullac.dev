7469f28680f62e3833d13b5d52b7974882de9695
gandomullac <gandomullac@gmail.com>
2025-04-02 14:37:02 +0200

    Disattivazione build rollup

--- vite.config.js
+++ vite.config.js
@@ -17,5 +17,5 @@ export default defineConfig({
       refresh: true,
     }),
   ],
-  build: { rollupOptions: { external: ["instant.page"] } },
+  //   build: { rollupOptions: { external: ["instant.page"] } },
 });
