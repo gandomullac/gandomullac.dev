eb95cf48ee2428193ff5d96e6fe3cbd2d8ce2aba
Claudio Gandini <claudio.gandini@tt-services.it>
2025-08-01 14:18:21 +0200

    chore: aggiunti strumenti code-quality

--- rector.php
+++ rector.php
@@ -3,34 +3,26 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Set\ValueObject\SetList;
-use RectorLaravel\Set\LaravelSetList;
+use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
+use Rector\Strict\Rector\Empty_\DisallowedEmptyRuleFixerRector;
 
 return RectorConfig::configure()
     ->withPaths([
         __DIR__.'/app',
-        __DIR__.'/bootstrap',
-        __DIR__.'/config',
-        __DIR__.'/lang',
+        __DIR__.'/bootstrap/app.php',
+        __DIR__.'/database',
         __DIR__.'/public',
-        __DIR__.'/resources',
-        __DIR__.'/routes',
-        __DIR__.'/tests',
     ])
-    // uncomment to reach your current PHP version
-    ->withPhpSets()
-    ->withTypeCoverageLevel(0)
-    ->withSets([
-        SetList::DEAD_CODE,
-        SetList::CODE_QUALITY,
-        SetList::CODING_STYLE,
-        SetList::PHP_81,
-
-        LaravelSetList::LARAVEL_110,
-        LaravelSetList::LARAVEL_ARRAY_STR_FUNCTION_TO_STATIC_CALL,
-        LaravelSetList::LARAVEL_CODE_QUALITY,
-        LaravelSetList::LARAVEL_ELOQUENT_MAGIC_METHOD_TO_QUERY_BUILDER,
-        LaravelSetList::LARAVEL_FACADE_ALIASES_TO_FULL_NAMES,
-        LaravelSetList::LARAVEL_LEGACY_FACTORIES_TO_CLASSES,
+    ->withSkip([
+        DisallowedEmptyRuleFixerRector::class,
+        // AddOverrideAttributeToOverriddenMethodsRector::class,
     ])
-    ->withRules([]);
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarations: true,
+        privatization: true,
+        earlyReturn: true,
+        strictBooleans: true,
+    )
+    ->withPhpSets();
